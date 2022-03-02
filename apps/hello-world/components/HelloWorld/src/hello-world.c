df751d7323599b773e0bec1a10b4b1a6436014e8
Andrew Kimball <andrewekimball@gmail.com>
2022-03-02 02:18:59 -0700

    Try simple insecure crypto library

--- apps/hello-world/components/HelloWorld/src/hello-world.c
+++ apps/hello-world/components/HelloWorld/src/hello-world.c
@@ -2,111 +2,60 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
 #include <aes.h>
-#include <rsa.h.in>
-#include <randapi.h>
+#include <rsa.h>
 
 int run(void) {
-  printf("\nTesting Tiny AES Library:\n");
-  const size_t BLOCK_LEN = 16; // Length of aes blocks in bytes.
-  struct AES_ctx ctx;
-  // Message and key are byte of 'Thats my Kung Fu'.
-  const uint8_t key[] = {
-    0x54, 0x68, 0x61, 0x74,
-    0x73, 0x20, 0x6D, 0x79,
-    0x20, 0x4B, 0x75, 0x6E,
-    0x67, 0x20, 0x46, 0x75,
-  };
-  AES_init_ctx(&ctx, key);
-  uint8_t message[] = {
-    0x54, 0x68, 0x61, 0x74,
-    0x73, 0x20, 0x6D, 0x79,
-    0x20, 0x4B, 0x75, 0x6E,
-    0x67, 0x20, 0x46, 0x75,
-  };
-  AES_ECB_encrypt(&ctx, message);
-  for (int i = 0; i < BLOCK_LEN; i++)
-    printf("%02X", message[i]);
-  printf("\nTesting Cryptography Library:\n");
-      int i;
-    unsigned long ran;
-    char m[RFS_WWW],ml[RFS_WWW],c[RFS_WWW],e[RFS_WWW],s[RFS_WWW],raw[100];
-    rsa_public_key_WWW pub;
-    rsa_private_key_WWW priv;
-    csprng RNG;
-    octet M= {0,sizeof(m),m};
-    octet ML= {0,sizeof(ml),ml};
-    octet C= {0,sizeof(c),c};
-    octet E= {0,sizeof(e),e};
-    octet S= {0,sizeof(s),s};
-    octet RAW= {0,sizeof(raw),raw};
+  printf("\nTesting Crypto Library:\n\n");
+	WORD key_schedule[60], idx;
+	BYTE enc_buf[128];
+	BYTE plaintext[2][16] = {
+		{0x6b,0xc1,0xbe,0xe2,0x2e,0x40,0x9f,0x96,0xe9,0x3d,0x7e,0x11,0x73,0x93,0x17,0x2a},
+		{0xae,0x2d,0x8a,0x57,0x1e,0x03,0xac,0x9c,0x9e,0xb7,0x6f,0xac,0x45,0xaf,0x8e,0x51}
+	};
+	BYTE ciphertext[2][16] = {
+		{0xf3,0xee,0xd1,0xbd,0xb5,0xd2,0xa0,0x3c,0x06,0x4b,0x5a,0x7e,0x3d,0xb1,0x81,0xf8},
+		{0x59,0x1c,0xcb,0x10,0xd4,0x10,0xed,0x26,0xdc,0x5b,0xa7,0x4a,0x31,0x36,0x28,0x70}
+	};
+	BYTE key[1][32] = {
+		{0x60,0x3d,0xeb,0x10,0x15,0xca,0x71,0xbe,0x2b,0x73,0xae,0xf0,0x85,0x7d,0x77,0x81,0x1f,0x35,0x2c,0x07,0x3b,0x61,0x08,0xd7,0x2d,0x98,0x10,0xa3,0x09,0x14,0xdf,0xf4}
+	};
+	int pass = 1;
 
-    time((time_t *)&ran);
+	// Raw ECB mode.
+	printf("* ECB mode:\n");
+	aes_key_setup(key[0], key_schedule, 256);
+	printf(  "Key          : ");
+        for(int idx = 0; idx < 32; idx++)
+                printf("%02x", key[0][idx]);
 
-    RAW.len=100;				/* fake random seed source */
-    RAW.val[0]=ran;
-    RAW.val[1]=ran>>8;
-    RAW.val[2]=ran>>16;
-    RAW.val[3]=ran>>24;
-    for (i=0; i<100; i++) RAW.val[i]=i;
+	for(idx = 0; idx < 2; idx++) {
+		aes_encrypt(plaintext[idx], enc_buf, key_schedule, 256);
+		printf("\nPlaintext    : ");
+                for(int idx = 0; idx < 16; idx++)
+                    printf("%02x", plaintext[idx]);
+		printf("\n-encrypted to: ");
+                for(int idx = 0; idx < 16; idx++)
+                    printf("%02x", enc_buf[idx]);
+		pass = pass && !memcmp(enc_buf, ciphertext[idx], 16);
 
-    CREATE_CSPRNG(&RNG,&RAW);   /* initialise strong RNG */
+		aes_decrypt(ciphertext[idx], enc_buf, key_schedule, 256);
+		printf("\nCiphertext   : ");
+                for(int idx = 0; idx < 16; idx++)
+                    printf("%02x", ciphertext[idx]);
+		printf("\n-decrypted to: ");
+                for(int idx = 0; idx < 16; idx++)
+                    printf("%02x", enc_buf[idx]);
+		pass = pass && !memcmp(enc_buf, plaintext[idx], 16);
 
-    printf("Generating public/private key pair\n");
-    RSA_WWW_KEY_PAIR(&RNG,65537,&priv,&pub,NULL,NULL);
-
-    printf("Encrypting test string\n");
-    OCT_jstring(&M,(char *)"Hello World\n");
-
-    OAEP_ENCODE(HASH_TYPE_RSA_WWW,&M,&RNG,NULL,&E); /* OAEP encode message m to e  */
-
-    RSA_WWW_ENCRYPT(&pub,&E,&C);     /* encrypt encoded message */
-    printf("Ciphertext= ");
-    OCT_output(&C);
-
-    printf("Decrypting test string\n");
-    RSA_WWW_DECRYPT(&priv,&C,&ML);   /* ... and then decrypt it */
-
-    OAEP_DECODE(HASH_TYPE_RSA_WWW,NULL,&ML);    /* decode it */
-    OCT_output_string(&ML);
-
-
-    if (!OCT_comp(&M,&ML))
-    {
-        printf("FAILURE RSA Encryption failed");
-        return 1;
-    }
-
-    printf("Signing message\n");
-    PKCS15(HASH_TYPE_RSA_WWW,&M,&C);
-
-    RSA_WWW_DECRYPT(&priv,&C,&S); /* create signature in S */
-
-    printf("Signature= ");
-    OCT_output(&S);
-
-    RSA_WWW_ENCRYPT(&pub,&S,&ML);
-
-    if (OCT_comp(&C,&ML))
-    {
-        printf("Signature is valid\n");
-    }
-    else
-    {
-        printf("FAILURE RSA Signature Verification failed");
-        return 1;
-    }
-
-    KILL_CSPRNG(&RNG);
-    RSA_WWW_PRIVATE_KEY_KILL(&priv);
-
-    OCT_clear(&M);
-    OCT_clear(&ML);   /* clean up afterwards */
-    OCT_clear(&C);
-    OCT_clear(&RAW);
-    OCT_clear(&E);
-
-    printf("SUCCESS\n");
-  printf("Done\n");
+		printf("\n");
+	}
+  if (pass) {
+    printf("\nSucceeded!\n");
+  } else {
+    printf("\nFailed!\n");
+  }
+  printf("\nDone\n");
   return 0;
 }
